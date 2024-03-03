ed51b11c05cc6ddc004ee2f30a87072736242a57
Steven Edwards <cureadvocate@gmail.com>
2024-03-02 20:39:23 -0500

    Create ngstrap.sh
    Add ngstrap.sh.
    
    Place in your `PATH`, preferably.
    
    `~/.local/bin` is a good spot.

---
+++ ngstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+ng new $1 --defaults --standalone --routing
+cd $1
+npm uninstall @types/jasmine jasmine-core karma karma-chrome-launcher karma-coverage karma-jasmine karma-jasmine-html-reporter
+rm -rf node_modules/
+npm install -D jest @types/jest
+
+cat <<EOF > jest.config.ts
+module.exports = {
+setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+};
+EOF
+
+npm install ng-mocks @testing-library/angular @testing-library/jest-dom jest-environment-jsdom --save-dev
+
+cat <<EOF > setup-jest.js
+import '@testing-library/jest-dom';
+EOF
+
+sed -i 's/karma/jest/' angular.json
+sed -i 's/jasmine/jest/' tsconfig.spec.json
+
+sed -i '/"assets"/,/"scripts"/d' angular.json
